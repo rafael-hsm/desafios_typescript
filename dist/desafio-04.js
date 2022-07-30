c189bf7d4cd83db60a0dba24bd4f424aaf95c286
Rafael Meireles <rafameireles2011@gmail.com>
2022-07-30 18:25:55 -0300

    Update desafio-04.js

--- dist/desafio-04.js
+++ dist/desafio-04.js
@@ -17,7 +17,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 // Todas as requisições necessárias para as atividades acima já estão prontas, mas a implementação delas ficou pela metade (não vou dar tudo de graça).
 // Atenção para o listener do botão login-button que devolve o sessionID do usuário
 // É necessário fazer um cadastro no https://www.themoviedb.org/ e seguir a documentação do site para entender como gera uma API key https://developers.themoviedb.org/3/getting-started/introduction
-/* var apiKey = '3f301be7381a03ad8d352314dcc3ec1d';
+/* var apiKey = '';
 let apiKey;
 let requestToken;
 let username;
